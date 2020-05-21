8485257f4b520d6f7df57f2702515046f30b8096
pyq <pyq225@gmail.com>
2020-05-22 03:28:31 +0800

    update animation

--- assets/script/logo.ts
+++ assets/script/logo.ts
@@ -6,13 +6,38 @@ export default class Logo extends cc.Component {
     nodes: Array<cc.Node> = [];
 
     onLoad() {
-        cc.tween(this.nodes[0])
-            .repeat(3, cc.sequence(cc.fadeOut(0.5), cc.fadeIn(0.5)))
+        let t = cc.tween;
+        t(this.nodes[0])
+            .repeat(3, cc.sequence(cc.fadeOut(0.2), cc.fadeIn(0.2)))
             .call(() => {
-                cc.tween(this.nodes[1])
-                    .to(0.5, { scaleX: 1 })
-                    .call(() => {})
-                    .start();
+                t().parallel(
+                    t(this.nodes[0])
+                        .repeatForever(
+                            cc.sequence(cc.fadeOut(0.2), cc.fadeIn(0.2))
+                        )
+                        .start(),
+                    t(this.nodes[1])
+                        .to(0.2, { scaleX: 1 })
+                        .call(() => {
+                            t().parallel(
+                                t(this.nodes[2])
+                                    .to(0.2, {
+                                        scale: 1,
+                                        position: cc.v2(421, 192),
+                                    })
+                                    .call(() => {})
+                                    .start(),
+                                t(this.nodes[3])
+                                    .to(0.2, {
+                                        scale: 1,
+                                        position: cc.v2(448, -34),
+                                    })
+                                    .call(() => {})
+                                    .start()
+                            );
+                        })
+                        .start()
+                );
             })
             .start();
     }
