e422f714da0cb2db150f1bba651d1ab376e0050e
pyq <pyq225@gmail.com>
2020-05-22 00:24:28 +0800

    update logo animation

--- assets/script/logo.ts
+++ assets/script/logo.ts
@@ -2,6 +2,18 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Logo extends cc.Component {
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property([cc.Node])
+    nodes: Array<cc.Node> = [];
+
+    onLoad() {
+        cc.tween(this.nodes[0])
+            .repeat(3, cc.sequence(cc.fadeOut(0.5), cc.fadeIn(0.5)))
+            .call(() => {
+                cc.tween(this.nodes[1])
+                    .to(0.5, { scaleX: 1 })
+                    .call(() => {})
+                    .start();
+            })
+            .start();
+    }
 }
