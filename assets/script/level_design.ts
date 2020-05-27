eb6e74dbec8f3961dbbffd40945a8bf3aff32c04
pyq <pyq225@gmail.com>
2020-05-28 03:23:12 +0800

    update level design animation

--- assets/script/level_design.ts
+++ assets/script/level_design.ts
@@ -7,8 +7,9 @@ export default class LevelDesign extends cc.Component {
     @property([cc.Node])
     separators: Array<cc.Node> = [];
 
-    anim_speed = 0.2;
-    current_level = 6;
+    anim_speed = 0.1;
+    // current_level = 6;
+    current_level = 1;
 
     left0: cc.Node = null;
     right0: cc.Node = null;
@@ -174,5 +175,114 @@ export default class LevelDesign extends cc.Component {
     }
 
     // 关卡+1(向左滑动)
-    next_level() {}
+    next_level() {
+        this.current_level++;
+        this.left0.active = true;
+        this.right0.active = true;
+        let t = cc.tween;
+        for (let i = 0; i < this.nodes.length; i++) {
+            let script = this.nodes[i].getComponent("level_item");
+            switch (script.index) {
+                case 0:
+                    script.set_level(this.current_level - 2);
+                    break;
+                case 1:
+                    t(this.nodes[i])
+                        .parallel(
+                            t().by(this.anim_speed, {
+                                position: cc.v2(-200, 0),
+                            }),
+                            t().to(this.anim_speed, { opacity: 0 })
+                        )
+                        .call(() => {
+                            this.nodes[i].setPosition(cc.v2(400, 0));
+                            script.set_index(4);
+                            script.set_level(this.current_level + 2);
+                            // t(this.nodes[i])
+                            //     .to(this.anim_speed, { opacity: 255 })
+                            //     .start();
+                        })
+                        .start();
+                    break;
+                case 2:
+                    t(this.nodes[i])
+                        .parallel(
+                            t().by(this.anim_speed, {
+                                position: cc.v2(-200, 0),
+                            }),
+                            t().to(this.anim_speed, { scale: 1 })
+                        )
+                        .call(() => {
+                            script.set_index(1);
+                        })
+                        .start();
+                    break;
+                case 3:
+                    t(this.nodes[i])
+                        .parallel(
+                            t().by(this.anim_speed, {
+                                position: cc.v2(-200, 0),
+                            }),
+                            t().to(this.anim_speed, { scale: 1.6 })
+                        )
+                        .call(() => {
+                            script.set_index(2);
+                        })
+                        .start();
+                    break;
+                case 4:
+                    t(this.nodes[i])
+                        .by(this.anim_speed, {
+                            position: cc.v2(-200, 0),
+                            opacity: 255,
+                        })
+                        .call(() => {
+                            script.set_index(3);
+                        })
+                        .start();
+                    break;
+            }
+        }
+        t(this.left2)
+            .by(this.anim_speed, {
+                position: cc.v2(-240, 0),
+                opacity: 255,
+            })
+            .call(() => {
+                t(this.left2)
+                    .by(0, { position: cc.v2(240, 0), opacity: -255 })
+                    .start();
+            })
+            .start();
+        t(this.right2)
+            .by(this.anim_speed, {
+                position: cc.v2(-240, 0),
+                opacity: -255,
+            })
+            .call(() => {
+                t(this.right2)
+                    .by(0, { position: cc.v2(240, 0), opacity: 255 })
+                    .start();
+            })
+            .start();
+        t(this.right3)
+            .by(this.anim_speed, { position: cc.v2(-160, 0), opacity: 255 })
+            .call(() => {
+                t(this.right3)
+                    .by(0, { position: cc.v2(160, 0), opacity: -255 })
+                    .start();
+            })
+            .start();
+        t(this.left1)
+            .by(this.anim_speed, {
+                position: cc.v2(-240, 0),
+                opacity: -255,
+            })
+            .call(() => {
+                t(this.left1)
+                    .by(0, { position: cc.v2(240, 0), opacity: 255 })
+                    .start();
+            })
+            .start();
+    }
 }
